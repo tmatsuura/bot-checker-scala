154b6adf6d1a6e04ec6022a2b04f53f19d0e6d34
tmatsuura <takuji.matsuura@icloud.com>
2020-02-11 15:48:33 +0900

    first commit

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "bot-checker-scala"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
+
+libraryDependencies ++= Seq(
+  "com.github.pathikrit" %% "better-files" % "3.8.0",
+  "com.nrinaudo" %% "kantan.csv" % "0.6.0",
+  "com.nrinaudo" %% "kantan.csv-generic" % "0.6.0",
+)
---
+++ src/main/scala/com/tmatsuura/BotChecker.scala
@@ -0,0 +1,45 @@
+package com.tmatsuura
+
+import better.files.File
+import kantan.csv._
+import kantan.csv.ops._
+
+case class KnownBot(
+    ua: String,
+    isActive: Short,
+    exceptionPattern: String,
+    extraFlag1: Short,
+    extraFlag2: Short,
+    startOfString: Short,
+    inactiveData: String,
+)
+
+class BotChecker private (checkers: Seq[String => Boolean]) {
+  def isBot(ua: String): Boolean =
+    checkers.exists(f => f(ua))
+}
+
+object BotChecker {
+  implicit val knownBotDecoder: RowDecoder[KnownBot] =
+    RowDecoder.decoder(0, 1, 2, 3, 4, 5, 6)(KnownBot.apply)
+
+  private val occurAtStart = (x: String) =>
+    (y: String) => y.toLowerCase startsWith x.toLowerCase
+  private val appearAnywhere = (x: String) =>
+    (y: String) => y.toLowerCase contains x.toLowerCase
+
+  def from(filePath: String): BotChecker = {
+    val checkers = File(filePath).toJava
+      .asCsvReader[KnownBot](rfc.withCellSeparator('|'))
+      .collect { case Right(a) => a }
+      .filter(p => p.isActive == 1)
+      .map(b =>
+        b.startOfString match {
+          case 1 => occurAtStart(b.ua)
+          case 0 => appearAnywhere(b.ua)
+        },
+      )
+      .toSeq
+    new BotChecker(checkers)
+  }
+}
